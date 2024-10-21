630dd023d218858756e4ed396164db4ada07de55
DESKTOP-RNOAK4B\admin <dosol48626@naver.com>
2024-10-21 17:02:42 +0900

    페이징 기능 너무 어렵다

--- src/main/java/com/dosol/jpademo/repository/BoardRepository.java
+++ src/main/java/com/dosol/jpademo/repository/BoardRepository.java
@@ -1,8 +1,14 @@
 package com.dosol.jpademo.repository;
 
 import com.dosol.jpademo.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    @Query("select  b from Board b where b.title like concat('%',:keyward,'%')order by b.bno desc")
+    Page<Board> searchAll(String keyward, Pageable pageable);
+    //Page<Board> findByTitle(String title, Pageable pageable);
 
 }
